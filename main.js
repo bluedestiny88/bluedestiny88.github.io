cdfdffb1ea47ff052b16a4faf8f425f4b5dd1ca2
Patrick <bluedestiny88@gmail.com>
2020-01-29 14:07:11 -0800

    WUT

--- main.js
+++ main.js
@@ -124,10 +124,10 @@ Guy.prototype.draw = function () {
 }
 
 
-AM.queueDownload("./img/todd.png");
-//AM.queueDownload("./img/guy.jpg");
+AM.queueDownload("./img/Todd.png");
+AM.queueDownload("./img/guy.jpg");
 AM.queueDownload("./img/scottpilgrim.png");
-//AM.queueDownload("./img/runningcat.png");
+AM.queueDownload("./img/runningcat.png");
 AM.queueDownload("./img/background.jpg");
 
 AM.downloadAll(function () {
